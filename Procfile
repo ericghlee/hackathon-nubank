51c88416ce2b5cbf0503997b931243b18758326a
André Missaglia <andre.missaglia@gmail.com>
2016-08-28 05:22:47 -0300

    heroku postgres.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py runserver 0.0.0.0:$PORT --noreload
+web: DJANGO_SETTINGS_MODULE=hackathon.settings.prod python manage.py runserver 0.0.0.0:$PORT --noreload
---
+++ hackathon/settings/prod.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd30j2mssmtmvui',
+        'USER': 'zvsrdmtvowdynw',
+        'PASSWORD': 'cvQ_yajXXkZLv5dkWlyNZ2M2N3',
+        'HOST': 'ec2-107-22-250-212.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
