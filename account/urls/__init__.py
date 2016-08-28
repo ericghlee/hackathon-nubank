35c924f240cecf5dba27b407d4f70f0c8add8481
Eric Lee <ericlee@Erics-MacBook-Air.local>
2016-08-28 00:31:50 -0300

    Adicao do model de cartao de credito

--- account/urls/__init__.py
+++ account/urls/__init__.py
@@ -4,6 +4,7 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^grupos/', include('account.urls.groups')),
+    url(r'^cards/', include('account.urls.cards')),
     url('^', include('django.contrib.auth.urls')),
 ]
 
