3875f20a31efb2424a56212d687a6d75cc090a6a
Victor Wichmann Raposo <vwraposo@gmail.com>
2016-08-28 10:11:39 -0300

    New Users

--- default-user.py
+++ default-user.py
@@ -6,4 +6,8 @@ django.setup()  # noqa
 
 from website.models import User
 
-User.objects.create_superuser('admin', 'admin@admin.net', 'Administrador Fodão', '12345678909', 'senha')
+User.objects.create_superuser('admin1', 'admin1@admin.net', 'Andre Missagli', '12345678909', 'senha')
+User.objects.create_superuser('admin2', 'admin2@admin.net', 'Eric Lee', '12345678910', 'senha')
+User.objects.create_superuser('admin3', 'admin3@admin.net', 'Germano Neuenfeld', '12345678911', 'senha')
+User.objects.create_superuser('admin4', 'admin4@admin.net', 'Jordana Faria', '12345678912', 'senha')
+User.objects.create_superuser('admin5', 'admin5@admin.net', 'Victor Raposo', '12345678913', 'senha')
