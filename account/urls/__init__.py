7808322dbf82fb7a46473485dd6252eb2b6e3c03
André Missaglia <andre.missaglia@gmail.com>
2016-08-28 00:02:57 -0300

    Corrige cores.

--- account/urls/__init__.py
+++ account/urls/__init__.py
@@ -3,6 +3,7 @@
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^grupos/$', include('account.urls.groups')),
+    url(r'^grupos/', include('account.urls.groups')),
+    url('^', include('django.contrib.auth.urls')),
 ]
 
--- hackathon/settings.py
+++ hackathon/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = 'website.User'
-
+LOGIN_REDIRECT_URL = '/account/grupos'
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
